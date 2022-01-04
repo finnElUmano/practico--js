349dd90f0f83fc2f2c379be686338443d7d08ffa
fercho <fercho1541@outlook.com>
2022-01-03 21:51:16 -0600

    terminando-JS--Just-styles

--- figuras.js
+++ figuras.js
@@ -4,7 +4,7 @@ console.group("Cuadrados");
 
 const  perimetroCuadrado = (lado) => lado * 4;
 
-const areaCuadrado = (lado)=> lado * lado;
+const areaCuadrado = (lado) => lado * lado;
 
 console.groupEnd();
 
@@ -20,9 +20,7 @@ console.groupEnd();
 
 // Codigo del Circulo
 console.group("Círculos");
-
 // Radio
-
 //Diámetro
 const diametroCirculo = (radio)=> radio * 2;
 
@@ -34,26 +32,73 @@ console.log("Pi es: " + PI);
 const perimetroCirculo = (radio)=> diametroCirculo(radio) * PI;
 
 //Area
-const areaCirculo = (radio, radio2)=> (radio * radio2) * PI;
+const areaCirculo = (radio)=> (radio * 2) * PI;
 
 console.groupEnd();
 
 // Interactuamos con html
-
+// cuadrado
 const calcularPerimetroCuadrado = ()=>{
 const input = document.getElementById("InputCuadrado");
 const value = input.value;
 
 const perimetro = perimetroCuadrado(value);
 alert(perimetro);
-
 };
 
 const calcularAreaCuadrado = ()=>{
-
 const input = document.getElementById("InputCuadrado");
 const value = input.value;
 
 const area = areaCuadrado(value);
+alert(area);
+};
+//
+// triangulo
+const calcularPerimetroTriangulo = ()=> {
+  const imput0 = document.getElementById("lado1");
+  const imput1 = document.getElementById("lado2");
+  const imput2 = document.getElementById("base");
+  const value0 = parseInt(imput0.value);
+  const value1 = parseInt(imput1.value);
+  const value2 = parseInt(imput2.value);
+
+  const perimetro = perimetroTriangulo(value0,value1,value2);
+  alert(perimetro);
+};
+//
+const calcularAreaTriangulo = ()=> {
+  const input0 = document.getElementById("base");
+  const input1 = document.getElementById("altura");
+  const value0 = parseInt(input0.value);
+  const value1 = parseInt(input1.value);
+  const area = areaTriangulo(value0,value1);
+  alert(area);
+}
+// 
+//  circulo
+//
+// Diametro
+const calcularDiametrocirculo = ()=> {
+const rad = document.getElementById("InputCirculo");
+const valor = rad.value;
+
+const diametro = diametroCirculo(valor);
+alert(diametro);
+};
+//
+const calcularPerimetroCirculo = ()=> {
+const per = document.getElementById("InputCirculo");
+const valor = per.value;
+
+const perimetro = perimetroCirculo(valor);
+alert(perimetro);
+};
+//
+const calcularAreaCirculo = ()=> {
+const are = document.getElementById("InputCirculo");
+const valor = are.value;
+
+const area = areaCirculo(valor);
 alert(area);
 };
